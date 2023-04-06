177d2c615cc968bf2278b80d020c775889754704
MCeciliaLuna <lunama.cecilia@gmail.com>
2023-04-05 21:53:04 -0300

    cambio de cuenta de cloudinary a cuenta propia de la empresa

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -3,16 +3,12 @@ const bcrypt = require('bcrypt')
 
 const crearUsuario = async (req,res) => {
   try {
-  const { nombre, apellido, direccion, telefono, email, contraseña, role} = req.body;
+  const { email, contraseña, role} = req.body;
   const contraseñaEncriptada = bcrypt.hashSync(contraseña, 10);
 
     const nuevoUsuario = new Usuario({
-      nombre,
-      apellido,
-      direccion,
-      telefono,
       email,
-      contraseña: contraseñaEncriptada,
+      contraseña: contraseñaEncriptada,git
       role
     })
        await nuevoUsuario.save()
@@ -22,21 +18,4 @@ const crearUsuario = async (req,res) => {
   }
 }
 
-const traerUsuarios = async(req,res) => {
-  const totalUsuarios = await Usuario.find()
-  res.json(totalUsuarios)
-}
-
-const eliminarUsuario = async (req,res) => {
-  const { _id } = req.params
-  try {
-    const usuarioEliminado = await Usuario.findByIdAndDelete(_id)
-    res.json({
-      message: `USUARIO ${usuarioEliminado} ELIMINADO correctamente`
-    })
-  } catch (error) {
-    res.status(error.code || 500).json({ message: error.message });
-  }
-}
-
-module.exports = { crearUsuario, traerUsuarios, eliminarUsuario }
+module.exports = { crearUsuario }
--- modelos/usuario.js
+++ modelos/usuario.js
@@ -2,45 +2,22 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const usuario = new Schema ({
-    nombre: {
-      type: String,
-      trim: true,
-      required: true,
-      uppercase: true
-    },
-    apellido: {
-      type: String,
-      trim: true,
-      required: true,
-      uppercase: true,
-    },
-    direccion: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-    telefono: {
-      type: Number,
-      trim: true,
-      required: true,
-    },
-    email: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-    contraseña: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-
-    role: {
-      type: String,
-      required: true,
-      enum: ["USER", "ADMIN"],
-      default: "USER",
-    },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  contraseña: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["USER", "ADMIN"],
+    default: "ADMIN",
+  },
   }
 )
 
